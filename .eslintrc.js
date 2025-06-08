2f3defb71956ec921c38bfd71fbebd103470caa7
Rahul Singh <contact.rahul1991@gmail.com>
2025-06-08 11:23:07 +0530

    updated lint script

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,12 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    jest: true,
-  },
   extends: [
-    'react-app',         // uses create-react-app base rules
-    'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:react-hooks/recommended',
-    'eslint:recommended',
-    // 'prettier'         // uncomment if using Prettier
+    'react-app',                 // includes react, react-hooks, jsx-a11y
+    'react-app/jest',            // for jest environment
+    // 'plugin:prettier/recommended', // optionally enable Prettier
   ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['react', 'jsx-a11y', 'react-hooks'],
   rules: {
-    // your custom rules go here
-    'react/prop-types': 'off', // disable if not using PropTypes
+    // add custom rules here
     'no-console': 'warn',
-    'react/react-in-jsx-scope': 'off', // not needed in React 17+
+    'react/prop-types': 'off',
   },
 };
